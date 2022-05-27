44683d924af8f0fb8621ed3d414dc2588984ac5a
lamiamostafa <lamiamostafa791@gmail.com>
2022-05-27 18:38:18 +0600

    useer colellection update api is added

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
-//middlewars
+
+const app = express();
+
+//middleware
 app.use(cors());
 app.use(express.json());
 
@@ -13,15 +17,39 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        const productCollection = client.db('manufacturer-website').collection('products');
-
+        const productCollection = client.db('manufacturer').collection('products');
+        const userCollection = client.db('manufacturer').collection('users');
         app.get('/product', async (req, res) => {
             const query = {};
             const cursor = productCollection.find(query);
             const products = await cursor.toArray();
             res.send(products);
 
+
         });
+
+
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await productCollection.findOne(query);
+            res.send(product);
+        });
+
+
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            // const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ result });
+        });
+
     }
     finally {
 
@@ -29,15 +57,11 @@ async function run() {
 
 }
 run().catch(console.dir);
-app.get('/', (req, res) => {
-    res.send("Hello from Doctor uncle");
 
+app.get('/', (req, res) => {
+    res.send("Running server")
 });
 
-app.get('/product', (req, res) => {
-
-})
-
 app.listen(port, () => {
-    console.log(`Doctors app listening on port ${port}`);
-})
+    console.log('Listening to port', port);
+});
